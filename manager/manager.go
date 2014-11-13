2fce197e0032302f33b0898694d4d20fb6ec54cf
Jayanth Chennamangalam <jayanthc@gmail.com>
2014-11-13 21:42:37 +0000

    Added Manager type and a couple of getter methods.

--- manager/manager.go
+++ manager/manager.go
@@ -7,11 +7,34 @@ import (
 	"fmt"
 )
 
+// Manager defines the manager struct
+type Manager struct {
+	crontabPath string
+	portNum     uint16
+}
+
+// CrontabPath is the getter function for crontabPath
+func (m *Manager) CrontabPath() string {
+	return m.crontabPath
+}
+
+// PortNum returns the port number
+func (m *Manager) PortNum() uint16 {
+	return m.portNum
+}
+
+// NewManager Creates a new manager
+func NewManager() *Manager {
+	return &Manager{crontabPath: "gron.go", portNum: 40000}
+}
+
 // ReadCrontab reads the crontab
 func ReadCrontab(crontabPath string) (x int, err error) {
 	x, err = 0, nil
+	m := NewManager()
 
-	fmt.Println("Reading crontab!")
+	fmt.Printf("Reading crontab file %s...\n", m.CrontabPath())
+	fmt.Println(m.PortNum())
 
-	return x, err
+	return
 }
