42686f95b5508763805fca56eaf90ca93884adb5
teacoder <rkrishna442@gmail.com>
2014-12-20 22:25:54 -0800

    Adding changes missed out on the last commit

--- eval/eval.go
+++ eval/eval.go
@@ -3,21 +3,11 @@ package eval
 import (
 	"fmt"
 	"github.com/teacoder/gron/manager"
-	_ "io/ioutil"
-	_ "math/rand"
 	"os/exec"
-	_ "strings"
 	"time"
 )
 
-type Exec struct {
-	id       uint32
-	atTime   int64
-	interval int64 //seconds
-	cmd      string
-}
-
-var execQueue [manager.MaxEntries]Exec
+const GRON_WILDCARD = 255
 
 // Execute runs the command passed to it
 func execute(cmd string) error {
@@ -34,67 +24,41 @@ func execute(cmd string) error {
 	return nil
 }
 
-// PopulateExecQueue converts the in-memory config to an exec queue format for
-// the evaluator to run over and execute.
-func populateExecQueue(entries [manager.MaxEntries]manager.Entry, count int) {
-
-	const SecondsInMin int = 60
-	const SecondsInHour int = 3600
-	const SecondsInDay int = 86400
-	var secondsTd int64
-
-	///XXX Let's not worry about the DOM, DOW etc for now.
+// Poll will be called periodicaly from the main loop.
+// It walks the execution queue and run any commands whose time has come
+func poll(entries [manager.MaxEntries]manager.Entry, count int) {
 	curTime := time.Now()
-
-	// There has to be a better way to do this.
-	// TODO Add UTs
-	secondsToday := SecondsInHour*curTime.Hour() + SecondsInMin*curTime.Minute()
+	curHour := curTime.Hour()
+	curMin := curTime.Minute()
+	curDow := int(curTime.Weekday())
+	_, _, curDay := curTime.Date()
+	curMonth := int(curTime.Month())
 
 	for i := 0; i < count; i++ {
 		e := entries[i].Get()
 		min := int(e["min"].(uint8))
 		hr := int(e["hr"].(uint8))
 		cmd := e["cmd"].(string)
-		secondsSched := SecondsInHour*hr + SecondsInMin*min
+		dow := int(e["dow"].(uint8))
+		dom := int(e["dom"].(uint8))
+		mon := int(e["mon"].(uint8))
 
-		// Scheduled in the future on the same day
-		if secondsSched > secondsToday {
-			secondsTd = int64(secondsSched - secondsToday)
-		} else { // Scheduled for the next day
-			secondsTd = int64(((SecondsInHour * 24) - secondsToday) + secondsSched)
-		}
-		execQueue[i] = Exec{100, curTime.Unix() + secondsTd, int64(SecondsInDay), cmd}
-		fmt.Printf("Running [%s] in %d seconds\n", cmd, secondsTd)
-	}
-}
-
-// Init initializes the internal data structures for the evaluator
-func initialize(entries [manager.MaxEntries]manager.Entry, count int) {
-	populateExecQueue(entries, count)
-}
-
-// Poll will be called periodicaly from the main loop.
-// It walks the execution queue and run any commands whose time has come
-func poll() {
-	for i := 0; i < len(execQueue); i++ {
-		if execQueue[i].id != 0 && time.Now().Unix() >= execQueue[i].atTime {
-			execute(execQueue[i].cmd)
-			//Now, schedule it for the next interval
-			execQueue[i].atTime += execQueue[i].interval
-			fmt.Printf("Executed [%s], next on %s \n",
-				execQueue[i].cmd,
-				time.Unix(execQueue[i].atTime, 0).String())
+		if (mon == curMonth || mon == GRON_WILDCARD) &&
+			(dom == curDay || dom == GRON_WILDCARD) &&
+			(dow == curDow || dow == GRON_WILDCARD) &&
+			(hr == curHour || hr == GRON_WILDCARD) &&
+			(min == curMin || min == GRON_WILDCARD) {
+			go execute(cmd)
+			fmt.Printf("%d : Executed [%s] \n", i, cmd)
 		}
 	}
 }
 
 // Run is the entry function into the evaluator. It runs for ever, polling the
-// execution queue every second
+// execution queue every minute
 func Run(entries [manager.MaxEntries]manager.Entry, count int) {
-	initialize(entries, count)
-
 	for {
-		time.Sleep(1000)
-		poll()
+		poll(entries, count)
+		time.Sleep(time.Minute)
 	}
 }
--- gron.go
+++ gron.go
@@ -12,18 +12,6 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	// @jayanthc : We should move the "Entry", and other common structures out of
-	// the manager package and into a common 'gron' realm. It'd be nice to keep these
-	// two loosely coupled.
-
-	// @teacoder: My idea was that eval would keep a reference to mgr that
-	// would enable it to get all entries using mgr.Entries() instead of having
-	// a special Get() method for each entry. From a design pov, I think it's
-	// the manager's job to keep the entries, and the evaluator's job to ask
-	// for it, rather than keep entries outside. That will also keep gron.go
-	// simple.
-	// PS: We should use issues for this sort of thing.
-
 	var crontab string
 	if len(os.Args) == 2 {
 		crontab = os.Args[1]
